5ccb0d51d30e3e6077233a57588ad2e2eb641d42
Ivan Khokhlov <mozhno@MacBook-Air-Mozhno.local>
2023-12-19 15:18:39 +0300

    change src

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,11 @@
-const goblin = document.querySelectorAll('.cell');
-const goblinImage = document.createElement('img');
+// TODO: write code here
 
-goblinImage.src = '/src/goblin.png';
+// comment this to pass build
+const unusedVariable = "variable";
 
-let numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-let randomNum = 0;
-let temp = 0;
+// for demonstration purpose only
+export default function demo(value) {
+  return `Demo: ${value}`;
+}
 
-setInterval(() => {
-  if (!numArray.length) {
-    numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-  }
-  randomNum = Math.floor(Math.random() * numArray.length);
-  temp = numArray[randomNum];
-  numArray = numArray.filter((element) => element !== temp);
-  goblin[temp].appendChild(goblinImage);
-}, 1000);
+console.log("app.js included");
