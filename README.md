0fadabffbe684e6695140041476785e96fe2a9a3
Ivan Khokhlov <mozhno@MacBook-Air-Mozhno.local>
2023-12-19 15:16:05 +0300

    change src

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# Webpack5
-
-[Руководство по настройке Webpack](https://webpack.js.org/guides/)
-[Руководство по настройке GitHub Actions](https://docs.github.com/en/actions/quickstart)
+[![Build status](https://ci.appveyor.com/api/projects/status/s1pp9r52d96wnqfo?svg=true)](https://ci.appveyor.com/project/sonic-wave/ajs-dom)
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,18 @@
-import Game from './Game';
+const goblin = document.querySelectorAll('.cell');
+const goblinImage = document.createElement('img');
 
-const game = new Game();
-game.init();
+goblinImage.src = '/src/goblin.png';
+
+let numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let randomNum = 0;
+let temp = 0;
+
+setInterval(() => {
+  if (!numArray.length) {
+    numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  }
+  randomNum = Math.floor(Math.random() * numArray.length);
+  temp = numArray[randomNum];
+  numArray = numArray.filter((element) => element !== temp);
+  goblin[temp].appendChild(goblinImage);
+}, 1000);
